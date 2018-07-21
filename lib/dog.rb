aa990109322379693db2ad927c5996b5bff55472
Christina Ng <christinasy317@gmail.com>
2018-07-21 04:29:28 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
 # dog.rb
+class Dog
+
+  def initialize(dog_name, dog_breed = "Mutt")
+    @name = dog_name
+    @breed = dog_breed
+  end
+
+  def name
+    @name
+  end
+
+  def breed
+    @breed
+  end 
+
+
+
+
+end
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,11 @@
-# person.rb
+class Person
+
+  def initialize(person_name)
+    @name = person_name
+  end
+
+  def name
+    @name
+  end
+
+end
